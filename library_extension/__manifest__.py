4f6e67e513f96ba520b5f33c801185d5e46f5fce
timothylanuza <45361970+timothylanuza@users.noreply.github.com>
2025-07-07 19:19:35 +0800

    Add files via upload

---
+++ library_extension/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Library Extensions",
+    "version": "17.0.1.0",
+    "depends": [
+        "library",
+    ],
+    "external_dependencies": {},
+    "author": "Odev Solutions",
+    "website": "https://www.odevsolutions.com",
+    "summary": """Library Extensions""",
+    "description": """
+        Extensions for the Library module.
+    """,
+    "category": "Extra Tools",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/library_book_category_views.xml",
+        "views/library_book_xpath_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ library_extension/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import library_book_inherit
+from . import library_book_category
